872929b573b1e1dfa6a6e65551df1002d4a3d4be
PranavChisti <pranav2024@ssim.ac.in>
2025-07-02 11:49:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from io import BytesIO
 
 st.set_page_config(layout="wide")
 st.title("Excel-Based Company Insights Dashboard")
